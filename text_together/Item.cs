ddfe8f3ecb16a1334fd036cdc813147f6a52ef39
yoonjin1127 <129118497+yoonjin1127@users.noreply.github.com>
2025-04-22 10:04:28 +0900

    feat: 아이템 데이터 추가
    아이템 데이터 추가

--- text_together/Item.cs
+++ text_together/Item.cs
@@ -27,6 +27,14 @@ namespace textRPG
 
     static class ItemData
     {
-        
+        public static List<Item> ItemPool { get; private set; } = new List<Item>()
+        {
+            new Item("고양이 발톱", new Effect("공격력", 3), "은밀하고 빠른 공격을 위한 장비. 간지러울 수도 있음.", 900, false, false),
+            new Item("오리 튜브", new Effect("방어력", 4), "물에도 뜨고, 적의 공격도 튕겨냅니다. 아마도요.", 1200, false, false),
+            new Item("용의 송곳니", new Effect("공격력", 10), "진짜 용의 송곳니인지 의심스러운, 그러나 강력한 무기.", 3000, false, false),
+            new Item("닌자 두건", new Effect("방어력", 6), "은신 +10, 간지 +50. 정작 방어력도 있다.", 2000, false, false),
+            new Item("버섯 갑옷", new Effect("방어력", 7), "푹신푹신. 스파르타인에게는 모욕적인 방어구.", 2500, false, false),
+            new Item("하트망치", new Effect("공격력", 5), "사랑을 담아 내려치는 무기. 정신적인 데미지는 추가.", 1800, false, false),
+        };
     }
 }
