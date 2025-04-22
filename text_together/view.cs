8a42189b7df946f9d0382a3dc70da07b80288593
guni0527 <kkh02527@naver.com>
2025-04-22 17:04:23 +0900

    feat : UI프레임 제작
    UI 프레임을  ┏━┗━┃┣ ┫┳┻ 로 제작하여 깔끔하게 나누었습니다

--- text_together/view.cs
+++ text_together/view.cs
@@ -1,92 +1,85 @@
 ﻿using System;
 using System.Text;
 
-namespace View
+namespace text_together
 {
-    internal class view
+    public class View
     {
-        static void Main(string[] args)
+        static void View1(string[] args)
         {
             // 콘솔크기            
-            Console.SetWindowSize(120, 50); // 콘솔창 크기 
-            Console.CursorVisible = false;
+            Console.SetWindowSize(120, 50); // 콘솔창 사이즈
+            Console.OutputEncoding = Encoding.UTF8; // 유니코드 적용
+            Console.CursorVisible = false;// 입력 숨겨주는거 
 
             //프레임 만들기
             DrawAsciiFrame();
 
-           
             Console.ReadLine();
         }
 
         // 프레임 그리는 그거
-        static void DrawAsciiFrame()
+        public static void DrawAsciiFrame()
         {
             Console.Clear();
 
             int width = 100;   // 전체 가로 길이 (콘솔 너비)
             int height = 40;   // 전체 세로 길이 (콘솔 높이)
-            int Y = 25;     // 상단/하단을 나누는 줄 위치 숫자를 줄이면 커지고 높이면 작아짐
-            int X = 50;     // 하단에서 좌/우 나누는 위치
+            int downY = 20;        // 상단/하단을 나누는 줄 위치
+            int highX = 50;        // 하단에서 좌/우 나누는 위치
 
             for (int y = 0; y < height; y++)
             {
                 for (int x = 0; x < width; x++)
                 {
-                    // 상단 맨 윗줄 or 하단 맨 아랫줄
-                    if (y == 0 || y == height - 1)
+                    // 상단 맨 윗줄
+                    if (y == 0)
                     {
-                        Console.Write("//");
-                        x++; // 칸 건너뛰기
+                        if (x == 0) Console.Write("┏");
+                        else if (x == width - 1) Console.Write("┓");
+                        else Console.Write("━");
                     }
-                    // 상단/하단 나누는 가로줄 (중간선)
-                    else if (y == Y)
+
+                    // 하단 맨 아랫줄
+                    else if (y == height - 1)
                     {
-                        Console.Write("//");
-                        x++;
+                        if (x == 0) Console.Write("┗");
+                        else if (x == width - 1) Console.Write("┛");
+                        else if (x == highX) Console.Write("┻");
+                        else Console.Write("━");
                     }
-                    // 하단에서 좌/우 나누는 세로줄
-                    else if (y > Y && (x == X - 1 || x == X))
+
+                    // 상단/하단 나누는 중간선
+                    else if (y == downY)
                     {
-                        Console.Write("//");
-                        x++;
+                        if (x == 0) Console.Write("┣");
+                        else if (x == width - 1) Console.Write("┫");
+                        else if (x == highX) Console.Write("┳");
+                        else Console.Write("━");
                     }
-                    // 좌측 / 우측 테두리
-                    else if (x == 0 || x == width - 2)
+
+                    // 하단의 좌/우 나누는 가운데 세로줄 (하단일 때만)
+                    else if (y > downY && x == highX)
                     {
-                        Console.Write("//");
-                        x++;
+                        Console.Write("┃");
                     }
-                    // 나머지는 빈 공간
+
+                    // 좌측 / 우측 세로줄
+                    else if (x == 0 || x == width - 1)
+                    {
+                        Console.Write("┃");
+                    }
+
+                    // 나머지 빈 공간
                     else
                     {
                         Console.Write(" ");
                     }
                 }
+
                 Console.WriteLine();
             }
         }
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-/////////////////////////////////////////
-//                                      /
-//                                      /
-// 상단 영역 /
-//                                      /
-////////////////////////////////////////
-//         //               //          /
-// 왼쪽 하단 //   오른쪽 하단           /
-//         //               //          /
-////////////////////////////////////////
