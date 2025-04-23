d355932c97d92897e628b1b324bbf55fca884a65
guni0527 <kkh02527@naver.com>
2025-04-23 16:24:26 +0900

    feat : 몬스터 아스키아트
    5가지 몬스터를 만들었습니다.
    예시 : Cat.Draw 불러오시면 되실겁니다

--- text_together/MonsterAscii.cs
+++ text_together/MonsterAscii.cs
@@ -10,56 +10,96 @@ public class MonsterAscii
 {
     public static class Cat //고양이
     {
+        public static readonly string[] Art = new string[]
+        {
+            "  /\\_/\\ ",
+            " (  -.-  ) ",
+            "/       \\ ",
+            "(  ___   )"
+        };
+
         public static void Draw()
         {
-            Console.WriteLine("  /\\_/\\ ");
-            Console.WriteLine(" (  -.-  ) ");
-            Console.WriteLine("/       \\ ");
-            Console.WriteLine("(  ___   )");
+            foreach (var line in Art)
+            {
+                Console.WriteLine(line);
+            }
             Console.WriteLine();
         }
     }
     public static class Dog //멍무이
     {
+        public static readonly string[] Art = new string[]
+        {
+            "  /‾‾‾\\  ",
+            " ( •ᴥ• ) ",
+            " /|___|\\ ",
+            "(_/   \\_)"
+        };
+
         public static void Draw()
         {
-            Console.WriteLine("  /‾‾‾\\  ");
-            Console.WriteLine(" ( •ᴥ• ) ");
-            Console.WriteLine(" /|___|\\ ");
-            Console.WriteLine("(_/   \\_)");
+            foreach (var line in Art)
+            {
+                Console.WriteLine(line);
+            }
             Console.WriteLine();
         }
     }
     public static class Bat //박쥐
     {
+        public static readonly string[] Art = new string[]
+        {
+            " /\\_/\\  ",
+            "(｡•ᴥ•｡)",
+            " /   \\  ",
+            " ^^ ^^ "
+        };
+
         public static void Draw()
         {
-            Console.WriteLine(" /\\_/\\  ");
-            Console.WriteLine("(｡•ᴥ•｡)");
-            Console.WriteLine(" /   \\  ");
-            Console.WriteLine(" ^^ ^^ ");
+            foreach (var line in Art)
+            {
+                Console.WriteLine(line);
+            }
             Console.WriteLine();
         }
     }
     public static class Duck //오리
     {
+        public static readonly string[] Art = new string[]
+        {
+            "  __    ",
+            "<(o )___",
+            " (   ._> ",
+            "  `---’ "
+        };
+
         public static void Draw()
         {
-            Console.WriteLine("  __    ");
-            Console.WriteLine("<(o )___");
-            Console.WriteLine(" (   ._> ");
-            Console.WriteLine("  `---’ ");
+            foreach (var line in Art)
+            {
+                Console.WriteLine(line);
+            }
             Console.WriteLine();
         }
     }
     public static class Snowman //신난눈사람
     {
+        public static readonly string[] Art = new string[]
+        {
+            "  (•ᴗ•)  ",
+            " <(   )> ",
+            "  /   \\  ",
+            "  \"   \"  "
+        };
+
         public static void Draw()
         {
-            Console.WriteLine("  (•ᴗ•)  ");
-            Console.WriteLine(" <(   )> ");
-            Console.WriteLine("  /   \\  ");
-            Console.WriteLine("  \"   \"  ");
+            foreach (var line in Art)
+            {
+                Console.WriteLine(line);
+            }
             Console.WriteLine();
         }
     }
